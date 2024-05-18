144893d3df946a54ace2d966c6d5e74bdc08dd4a
BIuesun <lukachimbashvili@gmail.com>
2024-05-18 22:08:03 +0400

    third commit

---
+++ Day 2/Homework 2/Arithmetics.py
@@ -0,0 +1,13 @@
+#creating codes which will add two numbers,subtract,multiply and divide them
+
+#adding
+print(9 + 1)
+
+#subtracting
+print(9 - 1)
+
+#multiplying 
+print(9 * 3)
+
+#dividing
+print(9 / 3)
---
+++ Day 2/Homework 2/Variables.py
@@ -0,0 +1,7 @@
+#creating variables, storing integers in each of them and adding them up
+
+item=1
+
+grades=4
+
+print(item + grades)
