8b665827918b037f1eb8646837f683c764458d4d
BIuesun <lukachimbashvili@gmail.com>
2024-05-28 20:30:52 +0400

    sixth commit

---
+++ Day 5/Class work/inputmore.py
@@ -0,0 +1,8 @@
+
+#declaring variables
+name=input("Please enter your name ")
+surname=input("Please enter your surname ")
+age=input("Please enter your age ")
+
+#displaying
+print("You are " + name + " " + surname + " " +age + " years old"  )
