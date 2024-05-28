a7c1cb8c3b14d66efd6ee4461f83027edee0aeea
BIuesun <lukachimbashvili@gmail.com>
2024-05-28 16:58:08 +0400

    fifth commit

---
+++ Day 4/Homework 4/more_variables.py
@@ -0,0 +1,35 @@
+#declaring 10 variables and doing arithmatical operations on them
+
+#declaringing variables
+
+age=69
+
+cost=3
+
+quanity=9
+
+money=592
+
+amount=34
+
+planes=17
+
+
+#using snake case
+per_cost=93
+
+amount_per=354
+
+popcorn_cost=150
+
+price_per_kg=2
+
+#doing arithmatical operations with them
+
+print(cost + per_cost)
+
+print(planes- age)
+
+print(price_per_kg * money)
+
+print(amount / quanity)
---
+++ Day 4/Homework 4/variables.py
@@ -0,0 +1,24 @@
+#creating variables and storing information about me in them
+
+#creating first variable where i will store my name
+
+name=("Luka")
+
+#creating second variable were i will store my surname
+
+surname=("Chumbashvili")
+
+#creating third variable where i will store my age
+
+age=15
+
+#creating fourth variable where i will store my adress
+
+adress=("guramishvilis gamziri 18a")
+
+#displaying them on the screen
+
+print(name)
+print(surname)
+print(age)
+print(adress)
