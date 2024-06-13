a42be67516b6c68fb92dcf2edc145449f90c1918
BIuesun <lukachimbashvili@gmail.com>
2024-06-13 13:44:18 +0400

    eleventh commit

---
+++ Day 10/Homework/for_loops.py
@@ -0,0 +1,31 @@
+#creating 10 for loops
+
+for i in range(10):
+    print("i love GOA")
+
+for i in range(20):
+    print("GOA is the best")
+
+for i in range(30):
+    print("I love proggraming")
+
+for i in range(40):
+    print(i)
+
+for i in range(50):
+    print("list "+ str(i))
+
+for i in range(60):
+    print("i ran out of ideas")
+
+for i in range(70):
+    print(str(i) + " ideas")
+
+for i in range(80):
+    print("why 10 examples of loops")
+
+for i in range(90):
+    print("loops are useful")
+
+for i in range(100):
+    print("its finally done")
