2adbebaee81d5ccba953471025b76b55b258ef44
BIuesun <lukachimbashvili@gmail.com>
2024-10-07 21:49:56 +0400

    fourtyforth commit

---
+++ Day 43/Classwork/main.js
@@ -0,0 +1,46 @@
+
+const friend1 = {
+    firstname: "Luka",
+    lastname: "Chumbashvili",
+    age: "15",
+    height: "175",
+    eyecolor: "brown"
+}
+
+
+const friend2 = {
+    firstname: "Luka",
+    lastname: "Chumbashvili",
+    age: "15",
+    height: "175",
+    eyecolor: "brown"
+}
+
+
+const friend3 = {
+    firstname: "Luka",
+    lastname: "Chumbashvili",
+    age: "15",
+    height: "175",
+    eyecolor: "brown"
+}
+
+console.log(friend1)
+console.log(friend2)
+console.log(friend3)
+
+console.log(friend1.height=173)
+console.log(friend1)
+
+console.log(friend2.eyecolor="red")
+console.log(friend2)
+
+console.log(friend3.height= 176)
+console.log(friend3)
+
+
+document.body.children[0].textContent = "Sup world";
+document.body.children[0].style.color = "blue"
+// roedsac saiti itvirteba browseri cmis DOM anu Document Object Models
+// javascript engine anu zrava aris is componetni romeic asrulebs javascriptis codebs
+// asetebia  Babylon.js  menonJS  Impact da sxva
