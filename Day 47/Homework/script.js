5dcf3cc0fea88a5e8f253571dcff5618e426c36d
BIuesun <lukachimbashvili@gmail.com>
2024-10-22 16:36:22 +0400

    fourtyeight commit

---
+++ Day 47/Homework/script.js
@@ -0,0 +1,21 @@
+// wutebis wamebad gadaqceva
+function seconds(minutes){
+    return minutes * 60;
+}
+// ori ricxvis jamis mesameze gayofa
+function numbers(n1,n2,n3){
+    return (n1+n2)/n3
+}
+console.log(numbers(1,2,3))
+
+
+// aris tu ara pirveli ricxvi meti meoreze
+function isFirstLarger(n1, n1) {
+    return n1 > n2;
+}
+
+// elementis damateba sityva element: tan
+function elem(element) {
+    return "Element: " + element;
+
+}
