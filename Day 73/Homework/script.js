fe4ff0e402d8bab2f962c37ce9d693f32b41446c
BIuesun <lukachimbashvili@gmail.com>
2025-01-20 18:37:50 +0400

    seventyfourth commit

---
+++ Day 73/Homework/script.js
@@ -0,0 +1,15 @@
+let human = {
+    name: "Luka",
+    age: 15,
+    gender: "male"
+  };
+  
+  console.log(`Their name is ${human.name}, they are ${human.age} years old, and their gender is ${human.gender}.`);
+
+ ///////////////////////
+  let human2 = {
+    name:prompt("Please input your name: "),
+    age:prompt("Please input your age: "),
+    gender:prompt("Please input your gender: ")
+  }
+  console.log(`Their name is ${human2.name}, they are ${human2.age} years old, and their gender is ${human2.gender}.`);
