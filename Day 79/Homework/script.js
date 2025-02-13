f4e8e0844fab2a7142e08a605fab76b2ad9db11f
BIuesun <lukachimbashvili@gmail.com>
2025-02-13 17:23:23 +0400

    eightyone

---
+++ Day 79/Homework/script.js
@@ -0,0 +1,11 @@
+function digitSum(number) {
+    let sum = 0;
+    while (number > 0) {
+        sum += number % 10;
+        number = Math.floor(number / 10);
+    }
+    return sum;
+}
+
+let num = 345;
+console.log(digitSum(num));
---
+++ Day 80/Homework/script.js
@@ -0,0 +1,43 @@
+// 1.
+let arr1 = [2, 3, 4];
+arr.unshift(1);  
+console.log(arr1);
+
+// 2.
+let ar2 = [1, 2, 3, 4];
+let firstElement = arr.shift();  
+console.log(arr2);
+console.log(firstElement); 
+
+// 3.
+let arr3 = [1, 2, 3, 5];
+arr3.splice(3, 0, 4);  
+console.log(arr3);
+
+// 4.
+let arr4 = [1, 2, 3, 4, 5];
+let slicedArr = arr4.slice(1, 4);  
+console.log(slicedArr); 
+console.log(arr4); 
+
+// 5.
+let arr5 = [1, 2, 3];
+let arr6 = [4, 5, 6];
+let mergedArr = arr5.concat(arr6);  
+console.log(mergedArr);
+
+// 6.
+let arr7 = [3, 1, 4, 2];
+arr8.sort();  
+console.log(arr7); 
+
+// 7.
+let arr8 = [1, 2, 3, 4];
+arr8.reverse();  
+console.log(arr8); 
+
+// 8.
+let arr9 = ["Hello", "World"];
+let str10 = arr9.join(" ");  
+console.log(str); 
+
